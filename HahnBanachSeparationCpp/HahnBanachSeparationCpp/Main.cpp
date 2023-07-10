4aed68f053ca8743d3f73c0e194434ff99c028ba
houssam lamlih <houssam.lamlih@hotmail.com>
2023-07-10 09:07:48 +0200

    Theoreme de Hahn Banach forme geometrique

--- HahnBanachSeparationCpp/HahnBanachSeparationCpp/Main.cpp
+++ HahnBanachSeparationCpp/HahnBanachSeparationCpp/Main.cpp
@@ -20,9 +20,25 @@ private:
 	int x, y;
 };
 
+typedef struct Line {
+public:
+	Line(Point _p1, Point _p2) : p1(_p1), p2(_p2){}
+
+	inline Point GetPoint1() { return p1; }
+	inline Point GetPoint2() { return p2; }
+
+
+private:
+	Point p1, p2;
+};
+
 typedef struct Polygon {
 public:
 	Polygon(){}
+
+	inline int GetNumOfPoints() { return points.size(); }
+	const inline Point GetPoint(int ind) { return points[ind]; }
+
 
 	void AddPoint(const Point point){ 
 		points.push_back(point);
@@ -43,27 +59,22 @@ public:
 	bool PointInside(const Point point) {
 		int numPoints = points.size();
 		if (numPoints < 3) {
-			// Polygon must have at least 3 points
-			return false;
+ 			return false;
 		}
 
-		// Initialize a ray outside of the polygon
 		Point rayStart(point.GetX(), point.GetY() - 1);
 		int intersections = 0;
 
-		// Iterate through each edge of the polygon
 		for (int i = 0; i < numPoints; i++) {
 			Point current = points[i];
 			Point next = points[(i + 1) % numPoints];
 
-			// Check if the ray intersects with the current edge
 			if ((current.GetY() > point.GetY()) != (next.GetY() > point.GetY()) &&
 				point.GetX() < ((next.GetX() - current.GetX()) * (point.GetY() - current.GetY()) / (next.GetY() - current.GetY()) + current.GetX())) {
 				intersections++;
 			}
 		}
 
-		// If the number of intersections is odd, the point is inside the polygon
 		return (intersections % 2) == 1;
 
 	}
@@ -94,8 +105,59 @@ private:
 };
 
 
+int orientation(Point p, Point q, Point r) {
+	double val = (q.GetY() - p.GetY()) * (r.GetX() - q.GetX()) - (q.GetX() - p.GetX()) * (r.GetY() - q.GetY());
+	if (val == 0) return 0; 
+	return (val > 0) ? 1 : 2; 
+}
+
+bool doIntersect(Point p1, Point q1, Point p2, Point q2) {
+	int o1 = orientation(p1, q1, p2);
+	int o2 = orientation(p1, q1, q2);
+	int o3 = orientation(p2, q2, p1);
+	int o4 = orientation(p2, q2, q1);
+
+	if (o1 != o2 && o3 != o4) return true;
+
+	// Special cases: segments are collinear and overlapping
+	if (o1 == 0 && orientation(p1, p2, q1) == 0 && orientation(p1, q2, q1) == 0)
+		return true;
+	if (o2 == 0 && orientation(p1, p2, q2) == 0 && orientation(p1, q1, q2) == 0)
+		return true;
+	if (o3 == 0 && orientation(p2, p1, q2) == 0 && orientation(p2, q1, q2) == 0)
+		return true;
+	if (o4 == 0 && orientation(p2, p1, q1) == 0 && orientation(p2, q2, q1) == 0)
+		return true;
+
+	return false; 
+}
+
+
+void DrawSmallestSegmentBetween(SDL_Renderer* renderer, Polygon pol1, Polygon pol2) {
+
+	Point p1 = pol1.GetPoint(0);
+	Point p2 = pol2.GetPoint(0);
+	for (int i = 0; i < pol1.GetNumOfPoints(); ++i) {
+		int j = (i + 1) % pol1.GetNumOfPoints();
+		for (int k = 0; k < pol2.GetNumOfPoints(); ++k) {
+			int l = (k + 1) % pol2.GetNumOfPoints();
+			if (!doIntersect(pol1.GetPoint(i), pol1.GetPoint(j), pol2.GetPoint(k), pol2.GetPoint(l))) {
+				p1 = pol1.GetPoint(i);
+				p2 = pol2.GetPoint(k);
+
+				std::cout << "Found" << std::endl;
+				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+				SDL_RenderDrawLine(renderer, p1.GetX(), p1.GetY(), p2.GetX(), p2.GetY());
+
+				return;
+			}
+		}
+	}
+
+}
+
 void DrawSeparationLine(SDL_Renderer* renderer, Polygon pol1, Polygon pol2) {
-	// Draw separation line code
+
 }
 
 int main() {
@@ -104,7 +166,6 @@ int main() {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window* window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, APP_WIDTH, APP_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr) {
-		// Handle window creation error
 		return 1;
 	}
 
@@ -125,17 +186,17 @@ int main() {
 
 	while (isRunning)
 	{
-		SDL_Event event;
-		SDL_PollEvent(&event);
+		SDL_Event e;
+		SDL_PollEvent(&e);
 
 
-		switch (event.type) {
+		switch (e.type) {
 		case SDL_QUIT:
 			isRunning = false;
 			break;
 
 		case SDL_KEYDOWN:
-			if (event.button.button == SDLK_SPACE) {
+			if (e.key.keysym.sym == SDLK_SPACE) {
 				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 				firstPolygon = true;
 				r = 255, g = 0, b = 0;
@@ -145,18 +206,18 @@ int main() {
 
 				SDL_RenderClear(renderer);
 			}
-			else if (event.button.button == SDLK_a) {
-				DrawSeparationLine(renderer, pol1, pol2);
+			if (e.key.keysym.sym == SDLK_a) {
+				std::cout << "Draw Separation Line" << std::endl;
+				DrawSmallestSegmentBetween(renderer, pol1, pol2);
 			}
 
 
 			break;
 
 		case SDL_MOUSEBUTTONUP:
-			// Handle mouse button up event
-			if (event.button.button == SDL_BUTTON_LEFT) {
-				int mouseX = event.button.x;
-				int mouseY = event.button.y;
+			if (e.button.button == SDL_BUTTON_LEFT) {
+				int mouseX = e.button.x;
+				int mouseY = e.button.y;
 
 				if (firstPolygon)
 					pol1.AddPoint(Point(mouseX, mouseY));
@@ -168,7 +229,7 @@ int main() {
 					std::cout << "Point is inside" << pol1.PointInside(p) << std::endl;
 				}
 
-				SDL_SetRenderDrawColor(renderer, r, g, b, 255); // Set the color values
+				SDL_SetRenderDrawColor(renderer, r, g, b, 255); 
 
 				SDL_RenderDrawPoint(renderer, mouseX, mouseY); 
 				for (int i = 1; i < POINT_SIZE; i++) {
@@ -178,7 +239,7 @@ int main() {
 					SDL_RenderDrawPoint(renderer, mouseX - i, mouseY + i);
 				}
 			}
-			else if (event.button.button == SDL_BUTTON_RIGHT)
+			else if (e.button.button == SDL_BUTTON_RIGHT)
 			{
 				if ((!pol1.IsConvex() && firstPolygon) || !pol2.IsConvex())
 					continue;
@@ -200,9 +261,8 @@ int main() {
 		default:
 			break;
 		}
-		// Render all objects here
 
-		SDL_RenderPresent(renderer); // Update the window to show the changes
+		SDL_RenderPresent(renderer); 
 	}
 
 
